8eaed72e567096d0b9a957fc3c69e39530ac5d0f
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-07-28 17:37:39 +0300

    Initial Commit

---
+++ Car/CarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarApp.swift
+//  Car
+//
+//  Created by Soha Ahmed Hamdy on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
